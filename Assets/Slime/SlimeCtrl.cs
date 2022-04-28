b1bbcc4529f5e66cd8b9b2f7cd1a7efbe83ccb21
陳 <thor.amber1995@gmail.com>
2022-04-28 22:05:01 +0900

    .

--- Assets/Slime/SlimeCtrl.cs
+++ Assets/Slime/SlimeCtrl.cs
@@ -4,63 +4,69 @@ using UnityEngine;
 
 public class SlimeCtrl : MonoBehaviour
 {
-    public float a;
-    public float b;
-    public float c;
+    public float jump_force;
 
-    private Transform transform;
     private Rigidbody rb;
-    private Vector3 chargingScale;
-    private bool isCharging;
+    private bool jumpable;
+    private Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
-        transform = GetComponent<Transform>();
-        rb = GetComponent<Rigidbody>();
-        chargingScale = Vector3.one;
-        isCharging = false;
+       rb = GetComponent<Rigidbody>();
+       jumpable = false;
+       animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(!isCharging)
+       if(jumpable && Input.GetKey(KeyCode.Space))
+       {
+            animator.SetBool("Charge",true);
+       }
+
+        if (jumpable && Input.GetKeyUp(KeyCode.Space) && (animator.GetCurrentAnimatorStateInfo(0).IsName("Slime_charge") || animator.GetCurrentAnimatorStateInfo(0).IsName("Slime_Full_Charge")) )
         {
-            transform.localScale = new Vector3(1.0f, 0.8f + (Mathf.Sin(Time.timeSinceLevelLoad * a) + 1) / 10.0f, 1.0f);
+            animator.SetBool("Charge", false);
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                rb.AddForce(new Vector3(jump_force * (Mathf.Abs(transform.localScale.y - 1.0f) + 0.5f), jump_force * (Mathf.Abs(transform.localScale.y - 1.0f) + 0.5f), 0.0f));
+            }
+            else if(Input.GetKey(KeyCode.RightArrow))
+            {
+                rb.AddForce(new Vector3(-(jump_force * (Mathf.Abs(transform.localScale.y - 1.0f) + 0.5f)), jump_force * (Mathf.Abs(transform.localScale.y - 1.0f) + 0.5f), 0.0f));
+            }else
+            {
+                rb.AddForce(new Vector3(0.0f, jump_force * 1.414f * (Mathf.Abs(transform.localScale.y - 1.0f) + 0.5f), 0.0f));
+            }
+        }
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Block")
+        {
+            jumpable = true;
         }
        
+    }
 
-        if(Input.GetKey(KeyCode.Space))
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Block")
         {
-            isCharging = true;
-            transform.localScale = chargingScale;
-            chargingScale.y = chargingScale.y > 0.5f ? chargingScale.y - b*Time.deltaTime : chargingScale.y;
+            jumpable = true;
         }
-        else
-        {
-           
 
-            if(isCharging)
-            {
-                if (Input.GetKey(KeyCode.LeftArrow))
-                {
-                    rb.AddForce(new Vector3( -c / chargingScale.y, c / chargingScale.y, 0.0f));
-                }
-                else if (Input.GetKey(KeyCode.RightArrow))
-                {
-                    rb.AddForce(new Vector3( c / chargingScale.y, c / chargingScale.y, 0.0f));
-                }
-                else
-                {
-                    rb.AddForce(new Vector3(0.0f, c / chargingScale.y, 0.0f));
-                }
-                   
-            }
-            chargingScale = Vector3.one;
-            isCharging = false;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Block")
+        {
+            jumpable = false;
         }
-   
-        
     }
 }
